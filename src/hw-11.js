8846dc95335993fe607b7b0e3b352c9c9bf84173
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-08-18 17:39:43 +0300

    add hw

--- src/hw-11.js
+++ src/hw-11.js
@@ -4,7 +4,6 @@
 // return new Promise((resolve, reject) => {
 //    setInterval(()=>{
 //     resolve(`Проміс перейшов в стан resolve через ${ms} мілісекунд`)
-//     reject('Проміс не перейшов у стан resolve')
 //    }, ms)
 
    
@@ -13,111 +12,83 @@
 
 // GetMiliseconds(3000).then((succes)=>{
 //     console.log(succes)
-// }).catch((error)=>{
-//   console.log(error)
 // })
 
 
  
 // №2
-// const users = [
+const users = [
  
-// { name: 'Mango', active: true },
+{ name: 'Mango', active: true },
  
-// { name: 'Poly', active: false },
+{ name: 'Poly', active: false },
  
-// { name: 'Ajax', active: true },
+{ name: 'Ajax', active: true },
  
-// { name: 'Lux', active: false },
+{ name: 'Lux', active: false },
  
-// ];
+];
  
-//  function toggleUserState(allUsers, userName){
- 
-// const updatedUsers = allUsers.map(user =>{
-//     user.name === userName ? { ...user, active: !user.active } : user
-// });
+ function toggleUserState(allUsers, userName){
+ return new Promise((resolve)=>{
 
-//  return new Promise((resolve)=>{
-//  resolve(console.table(updatedUsers))
+  const updatedUsers = allUsers.map(user =>{
+    return user.name === userName ? { ...user, active: !user.active } : user
+});
+ resolve(updatedUsers)
 
-// })
-// };
+})
+};
 
-// toggleUserState(users, 'Mango').then((succes)=>{
-//     console.log(succes)
-// })
+const logger = updatedUsers => console.table(updatedUsers);
+
+toggleUserState(users, 'Mango').then(logger)
+
 //  toggleUserState(users, 'Lux').then(logger);
 
 // №3
 
 
-// Перепишіть функцію makeTransaction()так, щоб вона не використовувала callback-функції onSuccessі onError, 
-// а прийняла весь один параметр transactionі повернула проміс.
- 
 function randomIntegerFromInterval(min, max){
- 
-return Math.floor(Math.random() * (max - min + 1) + min);
- 
-};
- 
-// Якщо randomIntegerFromInterval це не функція то чому ми знизу задаємо їй параметри???? 
+ return Math.floor(Math.random() * (max - min + 1) + min);
+ };
 
-function makeTransaction(transaction, onSuccess, onError){
+function makeTransaction(transaction){
  const delay = randomIntegerFromInterval(200, 500);
  
- setTimeout(() => {
- const canProcess = Math.random() > 0.3;
- if (canProcess) {
- onSuccess(transaction.id, delay);
- 
- }else{
- onError(transaction.id);
- 
-}}, delay);
- 
-
 return new Promise((resolve, reject)=>{
-  const logSuccess = (id, time) => {
-    resolve(console.log(`Транзакція ${id} оброблена за ${time}ms`))
-    };
+  setTimeout(() => {
+    const canProcess = Math.random() > 0.3;
+    if (canProcess) {
+    resolve({id:transaction.id, time:delay});
     
-const logError = id => {
-       reject(console.warn(`Помилка обробки транзакції ${id}. Спробуйте пізніше.`))
-        };
-        
-
-})
-};
- 
+    }else{
+    reject({id:transaction.id});
+    
+   }}, delay);
+});
+}
 
 
+const logSuccess = ({id, time}) => {
  
-// /*
+  console.log(`Транзакція ${id} оброблена за ${time} ms`);
+   
+  };
+   
+   
+  const logError = ({id})=> {
+   
+  console.warn(`Помилка обробки транзакції ${id}. Спробуйте пізніше.`);
+   
+  };
+   
  
-// * Працює так
+makeTransaction({ id: 70, time: 150 })
  
-// */
+.then(logSuccess)
  
-// makeTransaction({ id: 70, сума: 150 }, logSuccess, logError);
- 
-// makeTransaction({ id: 71, сума: 230 }, logSuccess, logError);
- 
-// makeTransaction({ id: 72, сума: 75 }, logSuccess, logError);
- 
-// makeTransaction({ id: 73, сума: 100 }, logSuccess, logError);
- 
-// /*
- 
-// * Повинно працювати так
- 
-// */
- 
-// makeTransaction({ id: 70, сума: 150 })
- 
-// .then(logSuccess)
- 
-// .catch(logError);
+.catch(logError);
  
  
 // makeTransaction({ id: 71, сума: 230 })
